856eaed2bd2d13892c4903c31457132fd0d5e20f
Dmitry <98899785+mdqst@users.noreply.github.com>
2024-11-05 11:57:10 +0300

    Update dev-apps.js
    Replaced execSync with a non-blocking alternative.

--- scripts/dev-apps.js
+++ scripts/dev-apps.js
@@ -1,4 +1,4 @@
-const {execSync} = require('child_process');
+const { exec } = require('child_process');
 
 const devApp = process.argv[2];
 
@@ -7,7 +7,15 @@ const sequentialExecution = async (...commands) => {
     return 0;
   }
 
-  execSync(commands.shift(), {stdio: 'inherit'});
+  await new Promise((resolve, reject) => {
+    exec(commands.shift(), { stdio: 'inherit' }, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(stdout);
+    });
+  });
 
   return sequentialExecution(...commands);
 };
@@ -15,4 +23,6 @@ const sequentialExecution = async (...commands) => {
 sequentialExecution(
   `pnpm turbo run build --filter=@${devApp}^...`,
   `pnpm turbo run dev --filter=@${devApp}`
-);
+).catch((error) => {
+  console.error('Error executing command:', error);
+});
